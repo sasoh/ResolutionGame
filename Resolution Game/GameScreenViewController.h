94efa7183462f4428a0679f1edfc10d5a5a264df
Sasho <sasoh@Alexanders-Mac.local>
2015-01-01 19:35:48 +0200

    Initial commit

---
+++ Resolution Game/GameScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameScreenViewController.h
+//  Resolution Game
+//
+//  Created by Alexander Popov on 1/1/15.
+//  Copyright (c) 2015 -. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface GameScreenViewController : ViewController
+
+@end
---
+++ Resolution Game/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Resolution Game
+//
+//  Created by Alexander Popov on 1/1/15.
+//  Copyright (c) 2015 -. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
